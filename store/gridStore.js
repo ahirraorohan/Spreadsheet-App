d12c82360fb5350104969ea17c73dccf92e644e5
ahirraorohan <150143780+ahirraorohan@users.noreply.github.com>
2024-08-26 08:49:28 +0530

    Update gridStore.js

--- store/gridStore.js
+++ store/gridStore.js
@@ -9,3 +9,21 @@ export const useGridStore = create((set) => ({
       return { cells: updatedCells };
     }),
 }));
+
+export const useGridStore = create((set) => ({
+  cells: Array(1000).fill({ content: '', alignment: 'left', fontSize: 12 }),
+  updateCell: (index, content) => {
+    set((state) => {
+      const updatedCells = [...state.cells];
+      updatedCells[index] = { ...updatedCells[index], content };
+      return { cells: updatedCells };
+    });
+  },
+  updateCellFormatting: (index, formatting) => {
+    set((state) => {
+      const updatedCells = [...state.cells];
+      updatedCells[index] = { ...updatedCells[index], ...formatting };
+      return { cells: updatedCells };
+    });
+  },
+}));
